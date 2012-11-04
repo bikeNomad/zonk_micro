88936f9e787cfba4c7e38c5f5fc8f71ea52d7f7a
Ned Konz <ned@bike-nomad.com>
2012-11-04 09:51:39 -0800

    Got compiler mostly working

--- Zonk/lib/zonk/grammar_nodes.rb
+++ Zonk/lib/zonk/grammar_nodes.rb
@@ -1,3 +1,4 @@
+require 'zonk'
 require 'treetop'
 
 # included by 
@@ -7,18 +8,27 @@ module Zonk
 module Grammar
 
 class ZonkGrammarNode < Treetop::Runtime::SyntaxNode
-  def self.output_stream
-    @output_stream ||= $stdout
-  end
-
-  def self.reserved_words
-    @reserved_words ||= %w(digital_input digital_output analog_input analog_output timer
-      ticker rule table task application when if then and true false
-      on ON On off OFF Off)
-  end
-
-  def self.ports
-    @ports ||= {}
+  class << self
+    def output_stream
+      @output_stream ||= $stdout
+    end
+    def output_stream=(s)
+      @output_stream = s
+    end
+    def reserved_words
+      @reserved_words ||= %w(digital_input digital_output analog_input analog_output timer
+        ticker rule table task application when if then and true false
+        on ON On off OFF Off)
+    end
+    def ports
+      @ports ||= {}
+    end
+    def reserved_word?(w)
+      reserved_words.include?(w)
+    end
+    def port_type(pn)
+      ports[pn]
+    end
   end
 
   def reserved_word?(w)
@@ -26,19 +36,21 @@ class ZonkGrammarNode < Treetop::Runtime::SyntaxNode
   end
 
   def port_type(pn)
-    self.class.ports[pn]
+    self.class.port_type(pn)
   end
   
   def remember_port(pn,pk)
     self.class.ports[pn] = pk
+    puts("remember #{pn} #{pk}")
   end
 
   def printf(*args)
     ZonkGrammarNode.output_stream.printf(*args)
   end
 
-  def instantiate
-    output_stream.printf("%s = %s.new", name, model_class.name)
+  def instantiate(owner=nil)
+    owner_str = owner ? ", #{owner.name.text_value}" : ''
+    printf("%s = %s.new('%s'%s)\n", name.text_value, model_class.name, name.text_value, owner_str)
   end
 end
 
@@ -47,12 +59,9 @@ class ApplicationNode < ZonkGrammarNode
     Application
   end
 
-  def instantiate
+  def instantiate(owner=nil)
     super
-    tasks.elements.each do |task|
-      task.instantiate
-      printf("%s.add_task(%s)\n", name, task.name)
-    end
+    tasks.elements.each { |task| task.instantiate(self) }
   end
 end
 
@@ -61,42 +70,36 @@ class TaskNode < ZonkGrammarNode
     Task
   end
 
-  def instantiate
+  def instantiate(owner=nil)
     super
-    ports.elements.each do |port|
-      port.instantiate
-      case port.model_class
-      when Ticker, Timer
-        printf("%s.add_timer(%s)\n", name, port.name)
-      else
-        printf("%s.add_port(%s)\n", name, port.name)
-      end
-    end
-    tables.elements.each do |table|
-      table.instantiate
-      printf("%s.add_table(%s)\n", name, table.name)
-    end
+    ports.elements.each { |port| port.instantiate(self) }
+    tables.elements.each { |table| table.instantiate(self) }
   end
 end
 
-class PortNode < ZonkGrammarNode
+class PortNode  < ZonkGrammarNode
   def model_class
     case elements.first.text_value
     when 'digital_input'
-      DigitalInput
+      Zonk::DigitalInputPort
     when 'digital_output'
-      DigitalOutput
+      Zonk::DigitalOutputPort
     when 'analog_input'
-      AnalogInput
+      Zonk::AnalogInputPort
     when 'analog_output'
-      AnalogOutput
+      Zonk::AnalogOutputPort
     when 'ticker'
-      Ticker
+      Zonk::Ticker
     when 'timer'
-      Timer
+      Zonk::Timer
     else
       raise "unknown port kind #{elements.first.text_value}"
     end
+  end
+
+  def initialize(*args)
+    super
+    remember_port(elements[2].text_value, elements[0].text_value)
   end
 end
 
@@ -104,18 +107,38 @@ class TableNode < ZonkGrammarNode
   def model_class
     Table
   end
+
+  def instantiate(owner=nil)
+    super
+    rules.elements.each { |rule| rule.instantiate(self) }
+  end
 end
 
 class RuleNode < ZonkGrammarNode
   def model_class
     Rule
   end
+
+  def instantiate(owner=nil)
+    super
+    printf("%s.set_event(%s)\n", name.text_value, event.as_args.join(', '))
+    printf("%s.set_conditions(%s)\n", name.text_value, condition.as_args.join(", "))
+    printf("%s.add_action(%s)\n",
+                         name.text_value,
+                         actions.elements.collect { |act| act.as_args }.join(", "))
+  end
 end
 
-class ActionNode < ZonkGrammarNode
+class WordNode < ZonkGrammarNode
+  def is_reserved
+    ZonkGrammarNode.reserved_word?(text_value)
+  end
+  def port_type
+    ZonkGrammarNode.port_type(text_value)
+  end
 end
 
-class NameNode < ZonkGrammarNode
+class NameNode < WordNode
 end
 
 # rule boolean_literal
@@ -132,9 +155,6 @@ module BooleanLiteralNode
   end
 end
 
-def parse(text)
-  parser = GrammarParser.new
+end
 end
 
-end
-end
